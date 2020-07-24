865a25513b68f6cbe86ad52217858c4e9d13f6c0
Starlight Romero <starlightromero@gmail.com>
2020-07-24 09:42:26 -0700

    Added abductionCount to StatusBar

--- Components/Display/StatusBar.js
+++ Components/Display/StatusBar.js
@@ -1,6 +1,6 @@
 /*  global
     Display, width, height, ship, textFont, textSize, textAlign, CENTER, LEFT
-    noStroke, fill, text, rect
+    noStroke, fill, text, rect, RIGHT
 */
 
 class StatusBar extends Display {
@@ -13,18 +13,22 @@ class StatusBar extends Display {
     this.currentStatus = ship.currentStatus
   }
 
+  abductionCount () {
+    textFont(this.font)
+    textSize(this.fontSize)
+    textAlign(RIGHT, CENTER)
+    noStroke()
+    fill(0)
+    text(`ABDUCT: ${ship.abductionCount.total}`, width - 20, this.y + 20)
+  }
+
   status () {
     textFont(this.font)
     textSize(this.fontSize)
     textAlign(CENTER, CENTER)
     noStroke()
     fill(0)
-    text(`STATUS: ${this.update()}`, width / 2, this.y + 20)
-  }
-
-  update () {
-    this.currentStatus = ship.currentStatus
-    return this.currentStatus
+    text(`STATUS: ${ship.currentStatus}`, width / 2, this.y + 20)
   }
 
   score () {
@@ -46,5 +50,6 @@ class StatusBar extends Display {
     this.background()
     this.score()
     this.status()
+    this.abductionCount()
   }
 }
